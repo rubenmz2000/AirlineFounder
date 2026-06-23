55343640296a8f9ba353a75a644cfa75a9260f00
Ruben Monteagudo <rubenmz2000@gmail.com>
2026-06-23 09:21:31 +0200

    LiveMap finished

--- src/front/airline-founder/src/services/marketService.ts
+++ src/front/airline-founder/src/services/marketService.ts
@@ -2,6 +2,6 @@ import apiClient from '../api/apiClient';
 import type { AircraftModel } from '../types/market';
 
 export const getAvailableModels = async (): Promise<AircraftModel[]> => {
-    const response = await apiClient.get<AircraftModel[]>('/Market');
+    const response = await apiClient.get<AircraftModel[]>('/api/Market');
     return response.data;
 };
